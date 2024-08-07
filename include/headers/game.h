92b854379eb4a08b085af4b62218f29e78c1d0b3
JaimmyShine <134523265+GargoKark@users.noreply.github.com>
2024-08-07 16:39:17 +0200

    upload
    :)

---
+++ include/headers/game.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+class Game{
+public:
+    Game();
+    ~Game();
+
+    void init(const char* title, int x, int y, int w, int h, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running();
+
+private:
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    int cnt = 0;
+};
